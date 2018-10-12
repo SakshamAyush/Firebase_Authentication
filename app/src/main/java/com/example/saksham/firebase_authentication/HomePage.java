f79998a6efd4911c4169dbc5a2cfc8caf683d3ca
SakshamAyush <saksham.barca@gmail.com>
2018-10-12 23:27:26 +0530

    .

--- app/src/main/java/com/example/saksham/firebase_authentication/HomePage.java
+++ app/src/main/java/com/example/saksham/firebase_authentication/HomePage.java
@@ -1,13 +1,33 @@
 package com.example.saksham.firebase_authentication;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomePage extends AppCompatActivity {
-
+    TextInputEditText email,pass;
+    Button login,signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+        //for see password option in UI, add design library in gradle (app)
+        //Add the line app:passwordToggleEnabled="true" in inputedittext
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomePage.this, SignUp.class);
+                startActivity(i);
+            }
+        });
     }
 }
